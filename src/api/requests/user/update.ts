905ad4f8d348b19f9f3ab0702c3662bec9eac670
Juliano Santos <128374665+sntoosk@users.noreply.github.com>
2024-05-16 12:54:32 -0300

    Atualizar o update.ts

--- src/api/requests/user/update.ts
+++ src/api/requests/user/update.ts
@@ -3,7 +3,7 @@ import { API } from "../..";
 import User from "../../../models/User";
 
 export async function update(id: number, user: User) {
-  const response = await axios.post(`${API}/users/${id}`, user);
+  const response = await axios.put(`${API}/users/${id}`, user);
   const { data } = response;
   console.log(data);
   return data;
