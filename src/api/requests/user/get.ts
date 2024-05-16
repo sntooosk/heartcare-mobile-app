3f90e6729c9a1ac2fe8a49faa4e1e168ca1c02c5
Juliano Santos <128374665+sntoosk@users.noreply.github.com>
2024-05-16 12:55:04 -0300

    Atualizar o get.ts

--- src/api/requests/user/get.ts
+++ src/api/requests/user/get.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API } from "../..";
 
 export async function get(id: number) {
-  const response = await axios.get(`${API}/users/${id}`,);
+  const response = await axios.get(`${API}/users/${id}`);
   const { data } = response;
   console.log(data);
   return data;
