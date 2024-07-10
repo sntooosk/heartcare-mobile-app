f025ad0bf6607397fecf1ac87bd771e427dc45f9
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-07-10 11:30:42 -0300

    firts commit

--- src/api/requests/Pressure/get.ts
+++ src/api/requests/Pressure/get.ts
@@ -3,7 +3,7 @@ import { API } from "../..";
 
 export async function get(idUser: number, token: string) {
   try {
-    const response = await axios.get(`${API}/pressure/?userId=${idUser}`, {
+    const response = await axios.get(`${API}/pressure/user/${idUser}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
