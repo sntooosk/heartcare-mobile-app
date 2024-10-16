b644563998df0f7ed590ce2c0623e11efac4c0cb
sntooos <Juliano.santos88@icloud.com>
2024-10-16 09:33:12 -0300

    firts commit

--- src/hooks/ToastProvider.tsx
+++ src/hooks/ToastProvider.tsx
@@ -1,23 +1,12 @@
 import React, { ReactNode, useCallback } from "react";
 import { Platform, Alert } from "react-native";
 
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import { ToastContext } from "../context/ToastContext";
 
 export const ToastProvider = ({ children }: { children: ReactNode }) => {
   const showToast = useCallback(
     (type: "success" | "error" | "info", message: string) => {
-      if (Platform.OS === "web") {
-        toast(message, {
-          type,
-          position: "top-right",
-          autoClose: 4000,
-        });
-      } else {
-        Alert.alert(type.charAt(0).toUpperCase() + type.slice(1), message);
-      }
+      Alert.alert(type.charAt(0).toUpperCase() + type.slice(1), message);
     },
     []
   );
@@ -27,7 +16,6 @@ export const ToastProvider = ({ children }: { children: ReactNode }) => {
       <ToastContext.Provider value={{ showToast }}>
         {children}
       </ToastContext.Provider>
-      {Platform.OS === "web" && <ToastContainer />}
     </>
   );
 };
